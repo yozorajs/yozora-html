5942f7ca8e7ac0288d1b653662e924fb0e54cf88
guanghechen <me.guanghechen@gmail.com>
2023-04-02 20:23:55 +0800

    :arrow_up:  chore: upgrade devDependencies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,38 +1,22 @@
-import createRollupConfigs from '@guanghechen/rollup-config-tsx'
+import { createRollupConfig, tsPresetConfigBuilder } from '@guanghechen/rollup-config'
 import path from 'node:path'
 
-const paths = {
-  basePath: path.resolve('src'),
-}
-
 export default async function rollupConfig() {
-  const { default: manifest } = await import(
-    path.resolve('package.json'),
-    { assert: { type: 'json' } },
-  )
-
-  const config = await createRollupConfigs({
+  const { default: manifest } = await import(path.resolve('package.json'), {
+    assert: { type: 'json' },
+  })
+  const config = await createRollupConfig({
     manifest,
-    pluginOptions: {
-      typescriptOptions: {
-        tsconfig: 'tsconfig.src.json',
-      },
-      postcssOptions: {
-        extract: 'index.css',
-        minimize: false,
-        sourceMap: false,
-        modules: false,
-        postcssUrlOptions: {
-          url: 'inline',
-          maxSize: 0.5, // 0.5 KB
-          basePath: paths.basePath,
-          fallback: function (asset) {
-            const url = asset.url.replace(/^[/]assets[/]/, '../assets/')
-            return url
-          },
-        },
-      },
+    env: {
+      sourcemap: false,
     },
+    presetConfigBuilders: [
+      tsPresetConfigBuilder({
+        typescriptOptions: {
+          tsconfig: 'tsconfig.src.json',
+        },
+      }),
+    ],
   })
   return config
 }
